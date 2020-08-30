8e2bef0748c577103a38ec929f76a24e593a6bca
Mohamed Konate <ing.mohamedkonate@gmail.com>
2020-08-30 08:04:25 +0000

    Modification des widgets en réponse à une entrée

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class TutorialHome extends StatelessWidget {
       ),
       // body is the majority of the screen.
       body: Center(
-        child: MyButton(),
+        child: Counter(),
       ),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Add', // used by assistive technologies
@@ -39,6 +39,7 @@ class TutorialHome extends StatelessWidget {
     );
   }
 }
+
 class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -62,4 +63,50 @@ class MyButton extends StatelessWidget {
   }
 }
 
+class CounterDisplay extends StatelessWidget {
+  CounterDisplay({this.count});
 
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Count: $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: onPressed,
+      child: Text('Increment'),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      ++_counter;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: <Widget>[
+      CounterIncrementor(onPressed: _increment),
+      CounterDisplay(count: _counter),
+    ]);
+  }
+}
